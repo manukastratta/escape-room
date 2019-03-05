54d862e0fe30cf1e5a537956c129f5b6dc1c0a8b
Emily O'Neal <eeoneal@stanford.edu>
2019-03-04 17:48:04 -0800

    grab it updates

--- evil/Assets/Evil Lab/Emily_Assets/Scripts/GrabIt.cs
+++ evil/Assets/Evil Lab/Emily_Assets/Scripts/GrabIt.cs
@@ -6,19 +6,17 @@ public class GrabIt : MonoBehaviour
 {
     public GameObject keyShape;
     public GameObject grabberCube;
-    private Vector3 originalSize;
 
 
     // Start is called before the first frame update
     private void Start()
     {
-        originalSize = transform.localPosition;
 
     }
 
     public void Grabit()
     {
-        keyShape.transform.localPosition = grabberCube.transform.position;
+        keyShape.transform.position = grabberCube.transform.position;
         keyShape.transform.parent = grabberCube.transform;
     }
 }
